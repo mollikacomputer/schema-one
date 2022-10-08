680f5572d7460e079c14d0451d1f547d4e79c9fd
mollikacomputer <mollikacomputer3@gmail.com>
2022-10-08 23:13:36 +0600

    get product rest api created

--- app.js
+++ app.js
@@ -7,7 +7,44 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res, next)=>{
-    res.send('Schema One App is running')
+    res.send('schema One app is runnint');
 });
 
+const productSchima = mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Please Provide a name for this product"]
+    }
+})
+const Product = mongoose.model('Product', productSchima );
+app.get('/api/v1/product', async(req, res, next)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json({
+            status:"success",
+            data:products
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:"fail",
+            message:"Can not get Data",
+            error:error.message,
+        })
+    }
+})
 module.exports = app;
+
+
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get('/', (req, res, next)=>{
+//     res.send('Schema One App is running')
+// });
+
+// module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const color = require('colors');
-const app = require('./app.js');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const app = require('./app');
 const port = process.env.PORT || 8080;
+
+mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
+    console.log(`Database connection is successful`);
+  })
 
 app.listen(port, ()=>{
     console.log(`Listening to port ${port}`.yellow.bold);
 })
+
+// const color = require('colors');
+// const app = require('./app.js');
+// const port = process.env.PORT || 8080;
+
+// app.listen(port, ()=>{
+//     console.log(`Listening to port ${port}`.yellow.bold);
+// })
